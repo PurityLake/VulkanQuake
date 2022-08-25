76c721df247a4ed024e1757f62a5d4a29920ed67
Robert O'Shea <robertoshea2k11@gmail.com>
2022-08-26 00:04:09 +0100

    Can create instances

--- Headers/VulkanQuakeApp.h
+++ Headers/VulkanQuakeApp.h
@@ -1,5 +1,5 @@
 /*
- * Vulkan Program
+ * Vulkan Quake
  *
  * Copyright (C) 2016 Valve Corporation
  * Copyright (C) 2016 LunarG, Inc.
@@ -19,13 +19,16 @@
 
 #pragma once
 
-#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
 
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
+#include <vector>
+
+#include "Utils.h"
 
 class VulkanQuakeApp {
 // ------------------------
@@ -34,13 +37,15 @@ class VulkanQuakeApp {
 public:
 	const uint32_t WIDTH = 1280;
 	const uint32_t HEIGHT = 720;
+	const std::string APP_NAME = "Vulkan Quake";
+	const std::string ENGINE_NAME = "Vulkan Quake";
 
 // ------------------------
 // Private members
 // ------------------------
 private:
 	SDL_Window* window;
-
+	VkInstance instance;
 
 // ------------------------
 // Public methods
@@ -60,7 +65,7 @@ private:
 	void InitWindow() {
 		if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 			std::cerr << "SDL failed to initialise: " << SDL_GetError() << std::endl;
-			throw std::exception("Failed to initialise SDL");
+			throw std::runtime_error("Failed to initialise SDL");
 		}
 		
 		window = SDL_CreateWindow("Vulkan Quake",
@@ -68,16 +73,49 @@ private:
 			WIDTH, HEIGHT, SDL_WINDOW_VULKAN);
 		if (window == nullptr) {
 			std::cerr << "SDL failed to create window: " << SDL_GetError() << std::endl;
-			throw std::exception("Failed to create SDL window");
+			throw std::runtime_error("Failed to create SDL window");
 		}
 
 		SDL_SetWindowResizable(window, SDL_FALSE);
 	}
 
 	// ######################
-	// InitVulakn
+	// InitVulkan
+	// ######################
 	void InitVulkan() {
-	
+		CreateInstance();
+	}
+
+	void CreateInstance() {
+		uint32_t extCount = 0;
+		SDL_Vulkan_GetInstanceExtensions(window, &extCount, nullptr);
+		std::vector<const char*> extensionNames(extCount);
+		SDL_Vulkan_GetInstanceExtensions(window, &extCount, extensionNames.data());
+
+		std::vector<const char*> layerNames{ };
+#ifdef DEBUG
+		layerNames.push_back("VK_LAYER_LUNARG_standard_validation");
+#endif
+
+		VkApplicationInfo appInfo{ };
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = APP_NAME.c_str();
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = APP_NAME.c_str();
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		VkInstanceCreateInfo createInfo{ };
+		createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		createInfo.pApplicationInfo = &appInfo;
+		createInfo.enabledExtensionCount = (uint32_t)extensionNames.size();
+		createInfo.ppEnabledExtensionNames = extensionNames.data();
+		createInfo.enabledLayerCount = (uint32_t)layerNames.size();
+		createInfo.ppEnabledLayerNames = layerNames.data();
+
+		if (utils::FunctionFailed(vkCreateInstance(&createInfo, nullptr, &instance))) {
+			throw std::runtime_error("Failed to create instance!");
+		}
 	}
 	
 	void MainLoop() {
@@ -92,6 +130,7 @@ private:
 	}
 	
 	void Cleanup() {
+		vkDestroyInstance(instance, nullptr);
 		if (window != nullptr) {
 			SDL_DestroyWindow(window);
 		}
